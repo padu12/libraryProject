19017acab318f3457e42b8b6423fcd2f63a8b563
Bohdan <notpadumink01@gmail.com>
2023-03-23 07:57:29 +0200

    added the project

---
+++ Lesson27.SpringJdbcTemplate/src/main/java/ru/alishev/springcourse/models/Book.java
@@ -0,0 +1,63 @@
+package ru.alishev.springcourse.models;
+
+public class Book {
+	private int book_id;
+	private int person_id;
+	private String name;
+	private String author;
+	private int year;
+
+	public Book(String name, String author, int year) {
+		super();
+		this.name = name;
+		this.author = author;
+		this.year = year;
+	}
+
+	public Book() {
+		super();
+	}
+
+	public int getPerson_id() {
+		return person_id;
+	}
+
+	public void setPerson_id(int person_id) {
+		this.person_id = person_id;
+	}
+
+	public int getBook_id() {
+		return book_id;
+	}
+
+
+	public void setBook_id(int book_id) {
+		this.book_id = book_id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+}
